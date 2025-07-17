02274432f0eb81be9c38c7f80af0e1441e859f65
rochmadnf <rochmadnf@gmail.com>
2025-07-17 10:42:48 +0800

    set logo app

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,5 +1,5 @@
 import { ImgHTMLAttributes } from 'react';
 
-export default function AppLogoIcon(props: ImgHTMLAttributes<HTMLImageElement>) {
-    return <img {...props} src="/assets/logo.png" />;
+export default function AppLogo(props: ImgHTMLAttributes<HTMLImageElement>) {
+    return <img {...props} src="/assets/img/logo.png" />;
 }
