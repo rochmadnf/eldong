b3796f41126887c5124e460377fc8d3b403e3ac5
rochmadnf <rochmadnf@gmail.com>
2025-08-18 21:38:59 +0800

    refactor code with prettier

--- routes/auth.php
+++ routes/auth.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::controller(\App\Http\Controllers\Auth\AuthenticatedSessionController::class)->group(function () {
-    Route::middleware('guest')->prefix('/dapur')->group(function () {
-        Route::get('/', 'index')->name('login');
-        Route::post('/', 'login');
-    });
+    Route::middleware('guest')
+        ->prefix('/dapur')
+        ->group(function () {
+            Route::get('/', 'index')->name('login');
+            Route::post('/', 'login');
+        });
 });
