7a695549e506835dace33d29337ca06bfc31e519
rochmadnf <rochmadnf@gmail.com>
2025-08-22 20:22:49 +0800

    fix typo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.env') === 'production') {
-            \Illuminate\Support\Facades\URL::forceScheme('htt[s');
+            \Illuminate\Support\Facades\URL::forceScheme('https');
         }
     }
 }
