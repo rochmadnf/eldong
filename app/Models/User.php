6cfdf4c1d9fd49b1b572f5c0dd8a15970c72af6b
rochmadnf <rochmadnf@gmail.com>
2025-09-28 16:16:57 +0800

    update users table and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,21 +3,22 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable, HasUuids;
+
 
     /**
-     * The attributes that are mass assignable.
+     * The attribute that can be assignment when create data
      *
      * @var list<string>
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['username', 'password'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -34,7 +35,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
