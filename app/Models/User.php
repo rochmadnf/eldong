c264d339d113c8c7db1aa3c1546ed9a81514fa64
rochmadnf <rochmadnf@gmail.com>
2025-09-28 16:18:17 +0800

    set uuid to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function uniqueIds(): array
+    {
+        return [
+            'uuid',
+        ];
+    }
 }
